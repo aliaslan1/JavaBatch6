828307f963a7b4c09812059ec1cc0db8beb804ed
aliaslan1 <aliaslanosh@gmail.com>
2020-03-25 16:47:44 -0500

    code for Q3

--- src/com/syntax/class13/Q3.java
+++ src/com/syntax/class13/Q3.java
@@ -2,4 +2,31 @@ package com.syntax.class13;
 
 public class Q3 {
 
+	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 * Write a Java Program to print first 10 numbers of Fibonacci series.
+		 * 0 1 1 2 3 5 8.....
+		 * 
+		 */
+		
+		int a,b,c;
+		
+		a=0;
+		b=1;
+		
+		for (int i=1; i<=10; i++) {
+			System.out.println(a+ " ");
+			
+			c=a+b;
+			a=b;
+			b=c;
+			
+		}
+		
+		
+		
+		
+	}
 }
